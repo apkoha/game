28832146c57ef07e6873558d9c446347c5f38b3b
apkoha <79258179+apkoha@users.noreply.github.com>
2024-10-08 20:31:52 +0300

    13

--- script.js
+++ script.js
@@ -1,55 +1,104 @@
+import CARDS from "./CARDS.json" with { type: "json" };
+
 const mainContainer = document.querySelector(".main__container");
-const cards = document.querySelectorAll(".game__card");
-const cardsInner = document.querySelectorAll(".card__inner");
+let datasetArr = [];
+let counter = 0;
+let endGameCounter = 0;
+let GameMoveCounter = 0;
+
+const doubleCardsArr = CARDS.concat(CARDS);
+
+const startGame = () => {
+  const cards = document.querySelectorAll(".game__card");
+  const targetDataset = event.target.parentElement.parentElement.dataset.color;
+  const cardContainer = event.target.parentElement.parentElement;
+
+  counter++;
+  GameMoveCounter++;
+  reverseCard();
+  cardContainer.removeEventListener("click", startGame);
+  cardContainer.style.cursor = "auto";
+  
+  for (let i = 0; i < cards.length; i++) {
+    if (cards[i].closest(".active") && targetDataset === datasetArr[datasetArr.length - 2]) {
+      cards[i].classList.add("open");
+      cards[i].children[0].classList.add("card__inner-open");
+      cards[i].classList.remove("active");
+      cards[i].children[0].classList.remove("card__inner-active");
+      cards[i].removeEventListener("click", startGame);
+      cards[i].style.cursor = "auto";
+      counter = 0;
+      endGameCounter++;
+      console.log(111);
+    }
+    
+    if (targetDataset != datasetArr[datasetArr.length - 2] && counter != 1) {
+      setTimeout(closeCard, 1000);
+      datasetArr = [];
+      counter = 0;
+    };
+  }
+
+  if (endGameCounter === 20) setTimeout(endGame, 600);
+  
+};
 
 const reverseCard = () => {
-  const target = event.target.parentElement;
-  const card = target.parentElement;
-  target.classList.add("card__inner-active");
-  card.classList.add("active");
+  const parentOfTarget = event.target.parentElement;
+  const cardOfTarget = parentOfTarget.parentElement;
+  const cardDataset = cardOfTarget.dataset.color;
+  
+  datasetArr.push(cardDataset);
+  parentOfTarget.classList.add("card__inner-active");
+  cardOfTarget.classList.add("active");
+};
 
-  // console.log('event.target: ', event.target.parentElement.parentElement);
-  for (let i = 0; i < cards.length; i++) {
-    // closeCard();
-    const dataset = cards[i].dataset.color;
-    const target = event.target.parentElement.parentElement;
-    const targetColor = event.target.parentElement.parentElement.dataset.color;
+const closeCard = () => {
+  const cards = document.querySelectorAll(".game__card");
+  const cardsInner = document.querySelectorAll(".card__inner");
 
-    // если карточка имеет класс active и её дата-атрибут совпадает с дата атрибутом ивентТаргета,
-    // то присвоить карточке и card__inner класс open
-    // if (cards[i].closest(".active") && dataset === targetColor) {
-    //   cards[i].classList.add("open");
-    //   target.classList.add("open");
-    //   // cards[i].classList.remove("active");
-    //   // target.classList.remove("active");
-    //   // reverseCard();
-    //   // closeCard();
-    //   console.log(111);
-    // }
+  cardsInner.forEach((item) => item.classList.remove("card__inner-active"));
+  cards.forEach(function(item) {
+    item.classList.remove("active");
+    if (!item.closest("open")) {
+      item.addEventListener("click", startGame);
+    }    
+  });
+};
+
+const endGame = () => {
+  alert("Ты выиграл! " + "Количесто ходов: " + (GameMoveCounter / 2));
+  mainContainer.innerHTML = "";
+  initGame();
+};
+
+const createCard = (cardData) => {
+  const cardContainer = document.createElement("div");
+  cardContainer.dataset.color = `${cardData.color}`;
+  cardContainer.className = "game__card";
+  cardContainer.innerHTML = `
+    <div class="card__inner">
+      <div class="card__front">
+        <img class="front__img" src="" alt="">
+      </div>
+      <div class="card__back">
+        <img class="back__img" src="${cardData.urlToImages}" alt="" width="120" height="100">
+      </div>
+    </div>
+  `
+  cardContainer.addEventListener("click", startGame);
+  mainContainer.append(cardContainer);
+}
+
+const getCardsData = () => {
+  for (const cardData of doubleCardsArr) {
+    createCard(cardData);
   }
 };
 
-// const reverseCard = () => {
-//   const target = event.target.parentElement;
-//   const card = target.parentElement;
-//   target.classList.add("card__inner-active");
-//   card.classList.add("active");
-// };
+const initGame = () => {
+  doubleCardsArr.sort(() => Math.random() - 0.5);
+  getCardsData(doubleCardsArr);
+};
 
-const closeCard = () => {
-  const target = event.target.parentElement.parentElement;
-  console.log('target: ', target);
-  const targetColor = event.target.parentElement.parentElement.dataset.color;
-  console.log('targetColor: ', targetColor);
-  // if () {
-  //   cards.forEach((item) => item.classList.remove("active"));
-  //   cardsInner.forEach((item) => item.classList.remove("active"));
-  // }
-  // for (let i = 0; i < cards.length; i++) {
-  //   console.log(222);
-  //   cards[i].classList.remove("active");
-  // }
-}
-
-mainContainer.addEventListener("click", reverseCard);
-mainContainer.addEventListener("click", closeCard);
+initGame();
